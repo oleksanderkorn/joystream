bfa02eb1b55d9814ddf5abdfd5ebfc79fcc5a7fd
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-03-03 15:03:37 +0100

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env sh
-# abort on errors
 set -e
-# build
 npm run build
-# navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-# git init
-git add -A
+git add .
 git commit -m 'Deploy to Github Pages'
-git push # -f git@github.com:oleksanderkorn/joystream-era-stats.git main:gh-pages
-cd -
+git push
+cd ..
