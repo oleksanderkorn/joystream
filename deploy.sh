cf65b94a6628b8728f20b1f2fba15e8f7c90d53a
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-03-03 11:51:26 +0100

    Cleanup.

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 # abort on errors
 set -e
 # build
-yarn run build
+npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'Deploy to Github Pages'
 git push -f git@github.com:oleksanderkorn/joystream-era-stats.git main:gh-pages
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/joystream/' : '/',
-    devServer: {
-        allowedHosts: [
-            'joystream.herokuapp.com'
-        ]
-    }
 }
