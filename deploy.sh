146d5f82d04ec7915be17d8aae958057d09931d3
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-05-31 10:36:02 +0200

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 set -e
 npm run build
 cd dist
+git init
 git add .
 git commit -m 'Deploy to Github Pages'
-git push --force
+git push -f git@github.com:oleksanderkorn/joystream.git master:gh-pages
 cd ..
